6f8401838df8a1085c163d120d5b250ad4c715b1
mjesus97 <mulavin@uc.cl>
2024-05-29 18:30:12 -0700

    Base del proyecto Spring

---
+++ src/test/java/com/alura/ChallengeLiteratura/ChallengeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.ChallengeLiteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
